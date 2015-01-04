5555989a5c7a759281392bf93ee4e62098f6a636
bryant tee <bryantt23@gmail.com>
2015-01-04 02:28:26 -0800

    b

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -2,7 +2,6 @@ require 'jumpstart_auth'
 
 class MicroBlogger
   attr_reader :client
-  
   def initialize
     # puts "Initializing MicroBlogger"
     puts "Initializing..."
@@ -12,9 +11,25 @@ class MicroBlogger
   def tweet(message)
     @client.update(message)
   end
-  
-  
+
+  def run
+    puts "Welcome to the JSL Twitter Client!"
+    command = ""
+    while command != "q"
+      printf "enter command: "
+      command = gets.chomp
+
+      case command
+      when 'q' then puts "Goodbye!"
+      else
+      puts "Sorry, I don't know how to #{command}"
+      end
+
+    end
+  end
+
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("MicroBlogger Initialized")
+# blogger.tweet("MicroBlogger Initialized")
+blogger.run
