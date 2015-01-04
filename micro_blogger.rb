78c8f29acd94b4f380b1ce2f15f232dc7c69dcd9
bryant tee <bryantt23@gmail.com>
2015-01-04 03:52:22 -0800

    fin which still doesn't work
    has an error due to windows, which could prob be fixed by google but idc
    
    https://www.google.com/search?q=%60rescue+in+perform%27%3A+SSL_connect+returned%3D1+errno%3D0+state%3DSSLv3+read+server+certificate+B%3A+certificate+verify+failed+(Twitter%3A%3AError)&oq=%60rescue+in+perform%27%3A+SSL_connect+returned%3D1+errno%3D0+state%3DSSLv3+read+server+certificate+B%3A+certificate+verify+failed+(Twitter%3A%3AError)&aqs=chrome..69i57.2794057j0j4&sourceid=chrome&es_sm=93&ie=UTF-8

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,94 +1,79 @@
 require 'jumpstart_auth'
-require 'klout'
-
+require 'bitly'
 
 class MicroBlogger
   attr_reader :client
+  
   def initialize
-    # puts "Initializing MicroBlogger"
-    puts "Initializing..."
+    puts "Initializing MicroBlogger"
     @client = JumpstartAuth.twitter
-    Klout.api_key = 'xu9ztgnacmjx3bu82warbr3h'
-
   end
-
+  
   def tweet(message)
-    @client.update(message)
+    if message.length > 140
+      puts "Message length is greater than 140; too long!"
+    else
+      @client.update(message)
+    end
   end
-=begin
+
+  def dm(target, message)
+    screen_names = @client.followers.map {|follower| @client.user(follower).screen_name}
+    unless screen_names.include?(target)
+      puts "Message target is not a follower! Cannot send."
+      return
+    end
+    puts "Trying to send #{target} this direct message:"
+    puts message
+    message = "d @#{target} #{message}"
+    tweet(message)
+  end
+
+  def followers_list
+    return @client.followers.map {|follower| @client.user(follower).screen_name}
+  end
+  
+  def spam_my_followers(message)
+    self.followers_list.each{|follower| dm(follower, message)}
+  end
+
+  def everyones_last_tweet
+    friends = @client.friends.sort_by {|friend| @client.user(friend).screen_name.downcase}
+    friends.each do |friend|
+      puts "----", "@#{@client.user(friend).screen_name} said this at #{@client.user(friend).status.created_at.strftime('%-m-%-d-%Y %l:%M %p')}", "---- \n"
+      puts "\n#{@client.user(friend).status.text}", "\n"
+    end
+  end
+
+  def shorten(original_url)
+    bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    puts "Shortening this URL: #{original_url}"
+    return bitly.shorten(original_url).short_url
+  end
 
   def run
     puts "Welcome to the JSL Twitter Client!"
     command = ""
     while command != "q"
       printf "enter command: "
-      command = gets.chomp
-
-      case command
-      when 'q' then puts "Goodbye!"
-      else
-      puts "Sorry, I don't know how to #{command}"
-      end
-
-    end
-  end
-=end
-
-
- def run
-    command = ""
-    while command != "q"
-      printf "Enter command: "
       input = gets.chomp
       parts = input.split(" ")
       command = parts[0]
       case command
-         when 'q' then puts "Goodbye!"
-         when 't' then tweet(parts[1..-1].join(" "))
-         else
-           puts "Sorry, I don't know how to #{command}"
+      when 'q' then puts "Goodbye!"
+      when 't' then tweet(parts[1..-1].join(" "))
+      when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+      when 'spam' then spam_my_followers(parts[1..-1].join(" "))
+      when 'elt' then everyones_last_tweet
+      when 'turl' then tweet(parts[1..-2].join(' ') + " #{shorten(parts[-1])}")
+      else
+        puts "Sorry, I don't know how to #{command}"
       end
     end
   end
 
-def dm(target, message)
-  puts "Trying to send #{target} this direct message:"
-  puts message
-  message = "d @#{target} #{message}"
-  tweet(message)
-end
-
-def everyones_last_tweet
-    friends = @client.friends
-    friends.each do |friend|
-      # find each friend's last message
-      # print each friend's screen_name
-      # print each friend's last message
-      puts ""  # Just print a blank line to separate people
-    end
-  end
-  
-def shorten(original_url)
-  # Shortening Code
-  puts "Shortening this URL: #{original_url}"
-  tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
-end
-
-  def klout_score
-    friends = @client.friends.collect{|f| f.screen_name}
-    friends.each do |friend|
-      # print your friend's screen_name
-      # print your friends's Klout score
-      # puts "" Print a blank line to separate each friend
-    end
-  end
-  
-  
-
 end
 
 blogger = MicroBlogger.new
-# blogger.tweet("MicroBlogger Initialized")
-blogger.run
-
-blogger.klout_score
+# blogger.run
+blogger.tweet("yoyoyo")
